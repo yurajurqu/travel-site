b2faab82f39a8f15539e058eabc4e3c9fab1d91c
Omar Barboza <yurajurqu@gmail.com>
2019-02-09 18:07:48 -0500

    browserfy

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,11 @@
-let gulp=require('gulp');
-let watch=require('gulp-watch');
-let postcss= require('gulp-postcss');
-let autoprefixer=require('autoprefixer'),
+let gulp=require('gulp'),
+watch=require('gulp-watch'),
+postcss= require('gulp-postcss'),
+autoprefixer=require('autoprefixer'),
 cssvars=require('postcss-simple-vars'),
 nested=require('postcss-nested'),
-cssimport=require('postcss-import');
+cssimport=require('postcss-import'),
+browserSync=require('browser-sync').create();
 
 gulp.task('default',function(){
     console.log('Task 1');
@@ -16,14 +17,23 @@ gulp.task('styles',function(){
     gulp.src('./app/assets/styles/styles.css')
     .pipe(postcss([cssimport,cssvars,nested,autoprefixer]))
     .pipe(gulp.dest('./app/temp/styles'));
-
 });
 gulp.task('watch',function(){
+    browserSync.init({
+        server: {
+            baseDir: 'app'
+        },
+        notify:false
+
+    });
     watch('./app/index.html',function(){
-        console.log('Index.html modified');
-        gulp.start('html');
+        browserSync.reload();
     });
     watch('./app/assets/styles/**/*.css',function(){
-        gulp.start('styles');
+        gulp.start('cssInject');
     });
+});
+gulp.task('cssInject',['styles'],function(){
+       gulp.src('./app/temp/styles/styles.css')
+     .pipe(browserSync.stream());
 });
